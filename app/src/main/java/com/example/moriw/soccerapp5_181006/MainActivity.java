456428ab1353bc0300285ce2d1c025ed7647bfa4
daimoriwaki <moriwaki.nodai@gmail.com>
2018-10-09 23:27:56 +0900

    ３回目コミット

--- app/src/main/java/com/example/moriw/soccerapp5_181006/MainActivity.java
+++ app/src/main/java/com/example/moriw/soccerapp5_181006/MainActivity.java
@@ -19,9 +19,9 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
